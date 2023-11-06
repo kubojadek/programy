1a1c2660a433a74007b8172263a8c906976d8765
kubojadek <jakub.turski.jt@gmail.com>
2023-11-06 08:28:09 +0100

    troll ##1

--- 30.10.23/ciąg-.c
+++ 30.10.23/ciąg-.c
@@ -6,16 +6,16 @@ main () {
     printf("podaj ile wyrazow tego ciągu a(i) = a(i-1)*√a(i-2)+a(i-3) chcesz zsumowac: ",N);
     scanf("%d",&N);
     float a[N],suma=0;
-    a[0]=1;
+    a[0]=2;
     a[1]=1.5;
-    a[2]=2;
-    for(int i=0;i<N;i++) {
-        if(i<3) {
-            suma=suma+a[i];
-        } else {
-            a[i]=a[i-1]*sqrt(a[i-2]+a[i-3]);
-            suma=suma+a[i];
-        }
+    a[2]=1;
+    for(int i=3;i<(N+3);i++) {
+        a[i]=a[i-1]*sqrt(a[i-2]+a[i-3]);
+        suma=suma+a[i];
+        
+    }
+    for(int j=3;j<(N+3);j++) {
+        printf("%f\n",a[j]);
 
     }
     printf("suma=%f",suma);

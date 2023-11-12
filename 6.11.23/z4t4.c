831f222663abdfa9aa721655a9ada008f3d7fb95
kubojadek <jakub.turski.jt@gmail.com>
2023-11-12 21:32:50 +0100

    z4

--- 6.11.23/z4t4.c
+++ 6.11.23/z4t4.c
@@ -4,19 +4,15 @@
 main() {
     float q,e,x,ps=1.0;
     int p=0,n;
-    printf("podaj liczby n q e:\n");
-    scanf("%d%f%f",&n,&q,&e);
+    printf("podaj liczby q n e:\n");
+    scanf("%f%d%f",&q,&n,&e);
     do {
         p++;
         for (int i=1;i<=p;i++) {
             ps=ps*i;
         }
         printf("p!= %f\n",ps);
-        if(n-q>0) {
-            x=(1/ps)*pow(n-q,p-1)*exp(n*q);
-        } else {
-            x=(1/ps)*(-pow(-(n-q),p-1))*exp(n*q);
-        }
+        x=(1/ps)*pow(n-q,p-1)*exp(n*q);
         printf("x= %f\n",x);
         ps=1.0;
     } while (x>e);

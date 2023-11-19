38a331873ec071860da86e76e6a7cb131a8fb6d3
kubojadek <jakub.turski.jt@gmail.com>
2023-11-19 20:01:18 +0100

    troll

--- 13.11.23/z2t5.c
+++ 13.11.23/z2t5.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 main() {
-    int x,y,z,licznik1=0,licznik=2;
+    int x,y,z=0,licznik1=0,licznik=2;
     printf("podaj liczbę: ");
     scanf("%d",&x);
     printf("podaj liczbę: ");
     scanf("%d",&y);
+    printf("podaj liczbę: ");
+    scanf("%d",&z);
 
-    while (x!=1 && y!=1 && z!=1) {
+    while (x!=1 || y!=1 || z!=1) {
+        x=y;
+        y=z;
         printf("podaj liczbę: ");
         scanf("%d",&z);
-            x=y;
-            y=z;
-            licznik++;
+        licznik++;
     }
     printf("wyczytano %d liczb",licznik);
 }

9d9de97b159151b3faa1c448f0307ee2c0c6a282
kubojadek <jakub.turski.jt@gmail.com>
2023-12-10 20:21:21 +0100

    d

--- 4.12.23/z3t7.c
+++ 4.12.23/z3t7.c
@@ -4,6 +4,7 @@ main() {
     FILE *plik;
     float x,min=0,max=0,krok;
     int licznik=0,N;
+    
     printf("podaj liczbę przedziałow N<=10:\n");
     scanf("%d",&N);
     int histogram[N];
@@ -30,24 +31,19 @@ main() {
             licznik++;
         }
     }
-    fclose(plik);
 
-    krok=max-min;
-    krok=krok/N;
+    krok=(max-min)/N;
     printf("max=%f min=%f krok=%f\n",max,min,krok);
 
-    plik=fopen("dane.txt","r");
-    if(plik==NULL) {
-        printf("nie znaleziono pliku");
-    } else {
-        while(feof(plik)==0){
-            fscanf(plik,"%f",&x);
-            for(int i=0;i<N;i++){
-                if(x>(min+(krok*i))&&x<(min+((i+1)*krok))) histogram[i]++;
-            }
+    rewind(plik);
+    while(feof(plik)==0){
+        fscanf(plik,"%f",&x);
+        for(int i=0;i<N;i++){
+            if(x>(min+(krok*i))&&x<(min+((i+1)*krok))) histogram[i]++;
         }
     }
     fclose(plik);
+
     for(int i=0;i<N;i++) {
         printf("%d ",histogram[i]);
     }
